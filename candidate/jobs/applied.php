20603283f2c51c44a3d42f7d5fee547210a1b9b9
amirali-ashraf <amiraliash.dev@example.com>
2020-08-04 16:34:24 -0400

    add date flter to applied jobs

--- candidate/jobs/applied.php
+++ candidate/jobs/applied.php
@@ -8,14 +8,21 @@ if (!isset($_SESSION['is_candidate']) && !$_SESSION['is_candidate']) {
     header('Location: ../../login.php');
 }
 
+$date_start = (empty($_POST['date_start'])) ? date('0-0-0') : $_POST['date_start'];
+$date_end = (empty($_POST['date_end'])) ? date('Y-m-d') : $_POST['date_end'];
+
 $job_records = $conn->prepare('SELECT *
     FROM gxc55311.z_applications
     JOIN gxc55311.z_jobs ON job_id = application_job_id
     JOIN gxc55311.z_employers ON job_employer_id = employer_id
-    where application_candidate_id = :application_candidate_id
-    ');
+    where application_candidate_id = :application_candidate_id AND
+    application_date >= :date_start AND
+    application_date <= :date_end'
+    );
 $user_id = $_SESSION['user_id'];
 $job_records->bindParam(':application_candidate_id', $user_id);
+$job_records->bindParam(':date_start', $date_start);
+$job_records->bindParam(':date_end', $date_end);
 $job_records->execute();
 
 
@@ -25,9 +32,52 @@ $job_records->execute();
 <?php require_once '../../partials/head-candidate.php' ?>
 
 <div class="container">
+
+
+</div>
+
+<div class="container">
     <h1>
         Jobs You Applied
     </h1>
+    <div class="row">
+        <div class="col-12">
+
+            <div class="card mb-5">
+                <div class="card-body">
+                    <h5 class="card-title">Search</h5>
+                    <form method="POST" action="./applied.php" class="form-group col-12">
+                        <div class="form-row align-items-center">
+                            <div class="col-5">
+                                <label class="sr-only" for="inlineFormInputGroup">From</label>
+                                <div class="input-group mb-2">
+                                    <div class="input-group-prepend">
+                                        <div class="input-group-text">From Search</div>
+                                    </div>
+                                    <input value="<?= isset($_POST["date_start"]) ? $_POST["date_start"] : date("Y-m-d") ?>" type="date" class="form-control" id="date_start" name="date_start" placeholder="Search">
+
+                                </div>
+                            </div>
+                            <div class="col-5">
+                                <label class="sr-only" for="inlineFormInputGroup">To</label>
+                                <div class="input-group mb-2">
+                                    <div class="input-group-prepend">
+                                        <div class="input-group-text">To</div>
+                                    </div>
+                                    <input value="<?= isset($_POST["date_end"]) ? $_POST["date_end"] : date("Y-m-d") ?>" type="date" class="form-control" id="date_end" name="date_end" placeholder="Search">
+
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <button type="submit" class="btn btn-primary btn-block mb-2">Filter</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div class="row">
         <div class="col-12">
             <table class="table">
@@ -49,7 +99,9 @@ $job_records->execute();
 
                         <tr>
                             <td><?= $row_count ?></td>
-                            <td><div title="<?= substr($row['job_description'],0,49) ?>"><?= $row['job_title'] ?></div></td>
+                            <td>
+                                <div title="<?= substr($row['job_description'], 0, 49) ?>"><?= $row['job_title'] ?></div>
+                            </td>
                             <td><?= $row['employer_name'] ?></td>
                             <td><?= $row['application_date'] ?></td>
                             <td>
@@ -115,7 +167,7 @@ $job_records->execute();
             </table>
         </div>
     </div>
-</div>
 
+</div>
 
 <?php require_once '../../partials/foot.php' ?>
