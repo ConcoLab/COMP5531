2541713d99b07aee64db3558f7529679ec1a5a81
amirali-ashraf <amiraliash.dev@example.com>
2020-08-04 14:49:13 -0400

    hot-fix

--- employer/membership/prime.php
+++ employer/membership/prime.php
@@ -1,14 +1,12 @@
 <?php
 require_once '../../partials/database.php';
 
-echo $_SESSION['user_id']."\n";
 $today = new DateTime(date('Y-m-d'));
 $end = new DateTime(date('Y-m-t'));
 $diff = date_diff($today, $end);
 $days_left = $diff->d;
 $days_passed = 30 - $days_left;
 
-echo $_SESSION["employer_category"];
 if ($_SESSION["employer_category"] != "Gold" && $_SESSION["employer_category"] != "Prime") {
     $stmt = $conn->prepare('SELECT * FROM gxc55311.z_users
                             WHERE user_id = :user_id LIMIT 1');
@@ -17,7 +15,6 @@ if ($_SESSION["employer_category"] != "Gold" && $_SESSION["employer_category"] !
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
     if ($result) {
         $current_balance = $result['user_balance'];
-        echo $current_balance;
         $balance = $current_balance - (50 / 30) * $days_left;
 
         $stmt_user = $conn->prepare('UPDATE gxc55311.z_users SET user_balance = :user_balance
@@ -46,9 +43,7 @@ if ($_SESSION["employer_category"] != "Gold" && $_SESSION["employer_category"] !
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
     if ($result) {
         $current_balance = $result['user_balance'];
-        echo $current_balance;
         $balance = $current_balance - (50 / 30) * $days_left + (100 / 30) * $days_left;
-        echo $balance;
         $stmt_user = $conn->prepare('UPDATE gxc55311.z_users SET user_balance = :user_balance
                             WHERE user_id = :user_id');
         $stmt_user->bindParam(':user_id', $_SESSION['user_id']);
