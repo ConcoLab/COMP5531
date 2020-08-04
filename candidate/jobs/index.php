3341ecd5e0a5a91a78e1d2a7c231aab37fb420d7
paul-ruban <paulruban@qq.com>
2020-08-03 21:13:51 -0400

    candidate search by categories

--- candidate/jobs/index.php
+++ candidate/jobs/index.php
@@ -8,6 +8,13 @@ if (!isset($_SESSION['is_candidate']) && !$_SESSION['is_candidate']) {
     header('Location: ../../login.php');
 }
 
+$stmt_status = $conn->prepare('SELECT user_status
+                                FROM gxc55311.z_users
+                                WHERE user_id = :user_id ;');
+$stmt_status->bindParam(':user_id', $_SESSION['user_id']);
+$stmt_status->execute();
+$status = $stmt_status->fetchColumn();
+
 $applied_count = $conn->prepare('SELECT COUNT(*)
 FROM gxc55311.z_applications
 WHERE application_candidate_id = :application_candidate_id
@@ -25,19 +32,44 @@ if($_SESSION["candidate_category"] == "Gold"){
     $can_apply = False;
 }
 
-$jobs_records = $conn->prepare('SELECT *
-FROM gxc55311.z_jobs
-JOIN gxc55311.z_employers ON job_employer_id = employer_id
-WHERE job_status = :job_status
-AND job_title LIKE :job_title
-AND job_id NOT IN (
-    SELECT application_job_id
-    FROM gxc55311.z_applications
-    WHERE application_candidate_id = :application_candidate_id
-)
-ORDER BY job_date_posted DESC
-');
 
+$job_categories = $conn->prepare('SELECT *
+FROM gxc55311.z_job_categories left join
+gxc55311.z_employers
+on job_category_employer_id=employer_id
+ORDER BY job_category_employer_id');
+$job_categories->execute();
+$jobs_records = null;
+if(!empty($_GET['category_searched'])){
+    $jobs_records = $conn->prepare('SELECT *
+                FROM gxc55311.z_jobs
+                JOIN gxc55311.z_employers ON job_employer_id = employer_id
+                WHERE job_status = :job_status
+                AND job_title LIKE :job_title
+                AND job_id IN (SELECT job_id FROM gxc55311.z_jobs_job_categories WHERE job_category_id = :job_category_id)
+                AND job_id NOT IN (
+                    SELECT application_job_id
+                    FROM gxc55311.z_applications
+                    WHERE application_candidate_id = :application_candidate_id
+                )
+                ORDER BY job_date_posted DESC
+                ');
+    $jobs_records->bindParam(':job_category_id', $_GET['category_searched']);
+}
+else{
+    $jobs_records = $conn->prepare('SELECT *
+                FROM gxc55311.z_jobs
+                JOIN gxc55311.z_employers ON job_employer_id = employer_id
+                WHERE job_status = :job_status
+                AND job_title LIKE :job_title
+                AND job_id NOT IN (
+                    SELECT application_job_id
+                    FROM gxc55311.z_applications
+                    WHERE application_candidate_id = :application_candidate_id
+                )
+                ORDER BY job_date_posted DESC
+                ');
+}
 $job_status = 'Active';
 $jobs_records->bindParam(':job_status', $job_status);
 $jobs_records->bindParam(':application_candidate_id', $_SESSION["user_id"]);
@@ -48,6 +80,9 @@ if (isset($_GET["jobTitle"])) {
 }
 $jobs_records->bindParam(':job_title', $job_title);
 $jobs_records->execute();
+
+
+
 
 require_once '../../partials/head-candidate.php' ?>
 
@@ -77,10 +112,23 @@ require_once '../../partials/head-candidate.php' ?>
                             <div class="input-group-prepend">
                                 <div class="input-group-text">Category</div>
                             </div>
-                            <select id="category" class="form-control">
-                                <option selected>Choose...</option>
-                                <option>Cat 1</option>
-                                <option>Cat 2</option>
+                            <select id="category" name="category_searched" class="form-control">
+                                <option selected disabled>Search by category</option>
+                                <?php
+                                    $current_emp = null;
+                                    echo '<optgroup label="General Categories">';
+                                    while ($row = $job_categories->fetch(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT)) {
+                                        if($row['employer_name'] == $current_emp){
+                                            $current_emp = $row['employer_name'];
+                                        }
+                                        else{
+                                            echo "</optgroup>";
+                                            $current_emp = $row['employer_name'];
+                                            echo '<optgroup label='.$row['employer_name'].'>';
+                                        }
+                                ?>
+                                    <option value=<?= $row['job_category_id'] ?>><?= $row['job_category_name'] ?></option>;
+                                <?php } ?>
                             </select>
                         </div>
                     </div>
@@ -114,8 +162,8 @@ require_once '../../partials/head-candidate.php' ?>
                 <p class="card-text"><?= $row['job_description'] ?></p>
                 <?php if ($can_apply) { ?>
                     <form method="POST" action="./apply.php">
-                        <input name="jobId" type="hidden" value="<?= $row['job_id'] ?>">
-                        <button class="btn btn-primary btn-block" type="submit">Apply</button>
+                        <input name="jobId" type="hidden" value="<?= $row['job_id'] ?>" >
+                        <button class="btn btn-primary btn-block" type="submit" <?php if($status != "Active"){ ?> disabled <?php }?>>Apply</button>
                     </form>
                 <?php } else { ?>
                     <hr>
