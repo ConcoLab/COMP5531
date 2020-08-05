173ac3ec2e70f43fba10c8cd08892a87b4618166
amirali-ashraf <amiraliash.dev@example.com>
2020-08-04 22:07:44 -0400

    hot fix

--- index.php
+++ index.php
@@ -1,76 +1,19 @@
 <?php
 require_once './partials/database.php';
 // require_once './partials/head-public.php';
-if(isset($_SESSION["is_candidate"]) && $_SESSION["is_candidate"]){
+if (isset($_SESSION["is_candidate"]) && $_SESSION["is_candidate"]) {
   header("Location: ./candidate/jobs");
-}else if (isset($_SESSION["is_employer"]) && $_SESSION["is_employer"]){
+} else if (isset($_SESSION["is_employer"]) && $_SESSION["is_employer"]) {
   header("Location: ./employer/jobs");
-}else if (isset($_SESSION["is_admin"]) && $_SESSION["is_admin"]){
+} else if (isset($_SESSION["is_admin"]) && $_SESSION["is_admin"]) {
   header("Location: ./admin");
 }
 ?>
 
-<div class="container">
-  <div class="row">
-    <?php if (isset($_SESSION['user_id'])) { ?>
-      <!-- <?php if (isset($_SESSION['is_candidate']) && $_SESSION['is_candidate']) { ?>
-        <div class="col-12 alert alert-success">
-          <h2>
-            Candidate
-          </h2>
-          <p>
-            Click more to search for the jobs in our job bank:
-          </p>
-          <a class="btn btn-success" href="./candidate/jobs">Candidate Panel</a>
-        </div>
-      <?php } else { ?>
-        <div class="col-12 alert alert-success">
-          <h2>
-            Candidate
-          </h2>
-          <p>
-            Click register to to be able to see the listed jobs.
-          </p>
-          <a class="btn btn-success" href="./candidate/jobs">Register as Job Candidate</a>
-        </div>
-      <?php } ?>
-
-      <?php if (isset($_SESSION['is_employer']) && $_SESSION['is_employer']) { ?>
-        <div class="col-12 alert alert-info">
-          <h2>
-            Employer
-          </h2>
-          <p>
-            Click more to go to the employer panel and post new jobs.
-          </p>
-          <a class="btn btn-info" href="./employer/jobs">Employer Panel</a>
-        </div>
-      <?php } else { ?>
-        <div class="col-12 alert alert-info">
-          <h2>
-            Employer
-          </h2>
-          <p>
-            Click register to to be able to see the listed jobs.
-          </p>
-          <a class="btn btn-info" href="./candidate/jobs">Register as Employer</a>
-        </div>
-      <?php } ?>
-
-      <?php if (isset($_SESSION['is_admin']) && $_SESSION['is_admin']) { ?>
-
-        <div class="col-12 alert alert-dark">
-          <h2>
-            Admin
-          </h2>
-          <p>
-            If you are an admin then you would be able to see the admin panel.
-          </p>
-          <a class="btn btn-dark" href="./admin/">Admin Panel</a>
-        </div>
-      <?php } ?> -->
-
-    <?php } else { ?>
+<?php if (!isset($_SESSION['user_id'])) { ?>
+  <?php require_once './partials/head-public.php' ?>
+  <div class="container">
+    <div class="row">
       <div class="col-12 alert alert-dark">
         <h2>
           Welcome
@@ -87,12 +30,11 @@ if(isset($_SESSION["is_candidate"]) && $_SESSION["is_candidate"]){
           </div>
 
         </div>
-
       </div>
-    <?php } ?>
+    </div>
+
+
 
   </div>
-</div>
-
-
-<?php require_once './partials/foot.php' ?>
+  <?php require_once './partials/foot.php' ?>
+<?php } ?>
